73e07723fd7d341481265bfddef9c1f5fd724027
Asim Ihsan <ssh@sshemail.ihsan.io>
2023-05-26 13:40:06 -0700

    0.6.0

--- Formula/code-digest.rb
+++ Formula/code-digest.rb
@@ -4,7 +4,7 @@ class CodeDigest < Formula
     url "https://github.com/asimihsan/code-digest/releases/download/0.6.0/code-digest-macos.zip"
     sha256 "8de92ef4f41172e593b07c3be750510bd13c57bb062d8325b110332e9e6c995f"
     license "MPL-2.0"
-    version "0.5.0"
+    version "0.6.0"
 
     def install
         bin.install "code-digest"
