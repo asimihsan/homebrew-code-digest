4c2f73049d1c35fa9690eaf24cde232b05fae0e3
Asim Ihsan <ssh@sshemail.ihsan.io>
2023-05-26 13:07:57 -0700

    0.5.0

--- Formula/code-digest.rb
+++ Formula/code-digest.rb
@@ -1,10 +1,10 @@
 class CodeDigest < Formula
     desc "Extract key content from source code files"
     homepage "https://github.com/asimihsan/code-digest"
-    url "https://github.com/asimihsan/code-digest/releases/download/0.4.0/code-digest-macos-arm64.zip"
-    sha256 "22366fbb6036894a7a93f113d6febc96d128867f0450a3b5166d1a08c17dfd8e"
+    url "https://github.com/asimihsan/code-digest/releases/download/0.5.0/code-digest-macos-arm64.zip"
+    sha256 "4efe8e35907fe884c723697204f8cba721b6e1f9b07e997e1e8692819a1b6257"
     license "MPL-2.0"
-    version "0.4.0"
+    version "0.5.0"
 
     def install
         bin.install "code-digest"
